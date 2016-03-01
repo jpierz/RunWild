cdded7aa338b23b06d196caba1581226c9981ea7
Jacob Pierzinski <pierzinskij@gmail.com>
2016-03-01 07:38:42 +0300

    Added ground texturing and object pooling for the ground spawning
    Added ground texturing and object pooling for the ground spawning

---
+++ Assets/Scripts/ObjectPooler.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ObjectPooler : MonoBehaviour {
+
+    public GameObject pooledObject;
+    public int amountObjects;
+
+    private List<GameObject> objects;
+
+    // Use this for initialization
+    void Start() {
+        objects = new List<GameObject>();
+
+        //Add non-active objects to a list
+        for (int i = 0; i < amountObjects; i++) {
+            GameObject obj = (GameObject) Instantiate(pooledObject);
+            obj.SetActive(false);
+            objects.Add(obj);
+        }
+	}
+
+    public GameObject GetPooledObject() {
+
+        //Searching for an inactive object
+        for (int i = 0; i < objects.Count; i++) {
+            if (!objects[i].activeInHierarchy) {
+                return objects[i];
+            }
+        }
+
+        //Adding a pooled object, no inactive ones were found
+        GameObject obj = (GameObject)Instantiate(pooledObject);
+        obj.SetActive(false);
+        objects.Add(obj);
+        return obj;
+
+    }
+	
+    
+}
---
+++ Assets/Scripts/PlatformGenerator.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformGenerator : MonoBehaviour {
+
+    //The ground to be spawned
+    public GameObject groundPlatform;
+
+    //The point where the spawn location is
+    public Transform spawnPoint;
+
+    //Width of the platform, to know how far ahead to add platform
+    private float platformWidth;
+
+    //Randomizing a little bit of distance
+    public int minGapDistance;
+    public int maxGapDistance;
+    private int gapRandomDistance;
+
+    //A reference to the objPooler class
+    public ObjectPooler objPooler;
+	// Use this for initializationd
+	void Start () {
+        //Getting the platform width
+        platformWidth = groundPlatform.GetComponent<BoxCollider2D>().size.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //If the PlatformGenerator is behind the spawnPoint
+	    if (transform.position.x < spawnPoint.position.x) {
+            //Calculating random distance
+            gapRandomDistance = Random.Range(minGapDistance, maxGapDistance);
+
+            //Updating the location of the PlatformGenerator and creating the spawn point for the new platform
+            transform.position = new Vector3(transform.position.x + platformWidth + gapRandomDistance, transform.position.y, transform.position.x);
+
+            //Getting a new object and updating its position. Setting rotation to the PlatformGenerator's rotation
+            //So it wont flip out
+            groundPlatform = objPooler.GetPooledObject();
+            groundPlatform.transform.position = transform.position;
+            groundPlatform.transform.rotation = transform.rotation;
+            groundPlatform.SetActive(true);
+        }
+	}
+}
